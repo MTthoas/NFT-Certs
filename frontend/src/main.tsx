a61a30b63325187e5f9d7f6ee7562856a6d8c13d
MtThoas <matthias.pecquery@gmail.com>
2025-02-12 19:17:59 +0100

    feat: wagmi hook for connect

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,119 +1,103 @@
 'use client'
 
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import { AxiosError } from 'axios'
+import { toast } from '@/hooks/use-toast'
+import { handleServerError } from '@/utils/handle-server-error'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createRouter, RouterProvider } from '@tanstack/react-router'
-import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { sepolia } from '@reown/appkit/networks'
-import { createAppKit } from '@reown/appkit/react'
-import { WagmiProvider } from 'wagmi'
-import { handleServerError } from '@/utils/handle-server-error'
-import { toast } from '@/hooks/use-toast'
+import { AxiosError } from 'axios'
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { createConfig, http, WagmiProvider } from 'wagmi'
+import { base, mainnet } from 'wagmi/chains'
+import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors'
 import { FontProvider } from './context/font-context'
 import { ThemeProvider } from './context/theme-context'
 import './index.css'
 import { routeTree } from './routeTree.gen'
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: (failureCount, error) => {
-        // eslint-disable-next-line no-console
-        if (import.meta.env.DEV) console.log({ failureCount, error })
+    defaultOptions: {
+        queries: {
+            retry: (failureCount, error) => {
+                // eslint-disable-next-line no-console
+                if (import.meta.env.DEV) console.log({ failureCount, error })
 
-        if (failureCount >= 0 && import.meta.env.DEV) return false
-        if (failureCount > 3 && import.meta.env.PROD) return false
+                if (failureCount >= 0 && import.meta.env.DEV) return false
+                if (failureCount > 3 && import.meta.env.PROD) return false
 
-        return !(
-          error instanceof AxiosError &&
-          [401, 403].includes(error.response?.status ?? 0)
-        )
-      },
-      refetchOnWindowFocus: import.meta.env.PROD,
-      staleTime: 10 * 1000, // 10s
+                return !(
+                    error instanceof AxiosError &&
+                    [401, 403].includes(error.response?.status ?? 0)
+                )
+            },
+            refetchOnWindowFocus: import.meta.env.PROD,
+            staleTime: 10 * 1000, // 10s
+        },
+        mutations: {
+            onError: (error) => {
+                handleServerError(error)
+
+                if (error instanceof AxiosError) {
+                    if (error.response?.status === 304) {
+                        toast({
+                            variant: 'destructive',
+                            title: 'Content not modified!',
+                        })
+                    }
+                }
+            },
+        },
     },
-    mutations: {
-      onError: (error) => {
-        handleServerError(error)
-
-        if (error instanceof AxiosError) {
-          if (error.response?.status === 304) {
-            toast({
-              variant: 'destructive',
-              title: 'Content not modified!',
-            })
-          }
-        }
-      },
-    },
-  },
 })
 
 // 1. Get projectId from https://cloud.reown.com
 const projectId = 'cb9a3d203b35a979abb8e955f45cdab1'
 
-// 2. Create a metadata object - optional
-const metadata = {
-  name: 'avax2',
-  description: 'AppKit Example',
-  url: 'https://reown.com/appkit', // origin must match your domain & subdomain
-  icons: ['https://assets.reown.com/reown-profile-pic.png'],
-}
-
-// 3. Set the networks
-const networks = [sepolia]
-
-// 4. Create Wagmi Adapter
-const wagmiAdapter = new WagmiAdapter({
-  networks,
-  projectId,
-  ssr: true,
-})
-
-// 5. Create modal
-createAppKit({
-  adapters: [wagmiAdapter],
-  networks,
-  projectId,
-  metadata,
-  features: {
-    analytics: true, // Optional - defaults to your Cloud configuration
-  },
+export const config = createConfig({
+    chains: [mainnet, base],
+    connectors: [
+        injected(),
+        walletConnect({ projectId }),
+        metaMask(),
+        safe(),
+    ],
+    transports: {
+        [mainnet.id]: http(),
+        [base.id]: http(),
+    },
 })
 
 // Create a new router instance
 const router = createRouter({
-  routeTree,
-  context: { queryClient },
-  defaultPreload: 'intent',
-  defaultPreloadStaleTime: 0,
+    routeTree,
+    context: { queryClient },
+    defaultPreload: 'intent',
+    defaultPreloadStaleTime: 0,
 })
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
+    interface Register {
+        router: typeof router
+    }
 }
 
 // Render the app
 const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
+    const root = ReactDOM.createRoot(rootElement)
 
-  root.render(
-    <StrictMode>
-      <WagmiProvider config={wagmiAdapter.wagmiConfig}>
-        <QueryClientProvider client={queryClient}>
-          <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
-            <FontProvider>
-              <RouterProvider router={router} />
-            </FontProvider>
-          </ThemeProvider>
-        </QueryClientProvider>
-      </WagmiProvider>
-    </StrictMode>
-  )
+    root.render(
+        <StrictMode>
+            <WagmiProvider config={config}>
+                <QueryClientProvider client={queryClient}>
+                    <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
+                        <FontProvider>
+                            <RouterProvider router={router} />
+                        </FontProvider>
+                    </ThemeProvider>
+                </QueryClientProvider>
+            </WagmiProvider>
+        </StrictMode>
+    )
 }
